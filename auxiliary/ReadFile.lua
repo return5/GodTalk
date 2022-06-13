f61f9104081b54e30153930e257a9533c8bbb96a
chris <armoredmoose@gmail.com>
2022-06-13 18:10:05 -0500

    created files, auxiliary directory, and brought in some library to help

---
+++ auxiliary/ReadFile.lua
@@ -0,0 +1,42 @@
+local ReadOnly = require("ReadOnlyTable")
+local open   = io.open
+local gmatch = string.gmatch
+local setmetatable = setmetatable
+local error = error
+
+local ReadFile = {}
+
+_ENV = ReadFile
+
+local function makeKeyValue(match,t,keyPat,valuePat)
+    local key = match:match(keyPat)
+    local value = match:match(valuePat)
+    t[key] = value
+end
+
+local function getContents(file,pat,readOnly,keyValue,keyPat,valuePat)
+    local tbl = {}
+    local makeValue =  keyValue and function(match,t) makeKeyValue(match,t,keyPat,valuePat)  end or function(match,t) t[#t + 1] = match end
+    for match in gmatch(file:read("a*"),pat) do
+        makeValue(match,tbl)
+    end
+    return readOnly and ReadOnly(tbl) or tbl
+end
+
+--read a file and put the contents into a table.
+--table is parsed based on pat.
+--if readonly is true then make resulting table a readOnly table.
+--if keyValue is true then file is treated as a list of key value pairs.
+--if text is a list of key,value pairs then keyPat is pattern to find the keys.
+--if text is a list of key,value pairs then valuePat is pattern to find the values.
+function ReadFile:new(filePath,pat,readOnly,keyValue,keyPat,valuePat)
+    if not filePath then error("filepath is nil\n") end
+    local file = open(filePath,"r")
+    if not file then error("could not open file at: "..filePath .. "\n") end
+    local contents = getContents(file,pat,readOnly,keyValue,keyPat,valuePat)
+    file:close()
+    return contents
+end
+
+
+return setmetatable(ReadFile,{__index = ReadFile,__call = ReadFile.new})
